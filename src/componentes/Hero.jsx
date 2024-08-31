9196ff58d6e7045a4bebb810e2780d1c827cda6d
Mostafijur <mrmoon025@gmail.com>
2024-08-31 14:15:46 +0600

    [feat] Changed profile pic.

--- src/componentes/Hero.jsx
+++ src/componentes/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/kevinRushProfile.png";
+import profilePic from "../assets/mostafijurProfile.png";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
@@ -50,7 +50,7 @@ const Hero = () => {
               initial={{ x: 100, opacity: 0 }}
               animate={{ x: 0, opacity: 1 }}
               transition={{ duration: 1, delay: 1.2 }}
-              className=""
+              className="rounded-2xl w-[500px] h-[500px]"
               src={profilePic}
               alt="Mostafijur Rahman"
             />
